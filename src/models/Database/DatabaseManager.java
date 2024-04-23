846a6c6b5ed87c5a02bbaa43af2ebcc8ea64a4e5
vncorrea <vinicorreag2001@gmail.com>
2024-04-23 20:16:56 -0300

    feat: ajustes

--- src/models/Database/DatabaseManager.java
+++ src/models/Database/DatabaseManager.java
@@ -2,7 +2,6 @@ package models.Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,13 +13,16 @@ public class DatabaseManager {
 
             statement.execute("CREATE TABLE IF NOT EXISTS livro_status (id_livro_status INTEGER PRIMARY KEY, descricao TEXT)");
             statement.execute("CREATE TABLE IF NOT EXISTS livro_categoria (id_livro_categoria INTEGER PRIMARY KEY, descricao TEXT)");
-            statement.execute("CREATE TABLE IF NOT EXISTS livro (id_livro INTEGER PRIMARY KEY, titulo TEXT, autor TEXT, editora TEXT, sinopse TEXT, paginas INTEGER, isbn TEXT, data_publicacao DATE, data_cadastro DATE id_livro_categoria INTEGER, id_livro_status INTEGER, FOREIGN KEY (id_livro_categoria) REFERENCES livro_categoria(id_livro_categoria), FOREIGN KEY (id_livro_status) REFERENCES livro_status(id_livro_status))");
+            statement.execute("CREATE TABLE IF NOT EXISTS livro (id_livro INTEGER PRIMARY KEY, titulo TEXT, autor TEXT, editora TEXT, sinopse TEXT, paginas INTEGER, isbn TEXT, data_publicacao DATE, data_cadastro DATE, id_livro_categoria INTEGER, id_livro_status INTEGER, FOREIGN KEY (id_livro_categoria) REFERENCES livro_categoria(id_livro_categoria), FOREIGN KEY (id_livro_status) REFERENCES livro_status(id_livro_status))");
             statement.execute("CREATE TABLE IF NOT EXISTS pessoa (id_pessoa INTEGER PRIMARY KEY, nome TEXT, logradouro TEXT, cpf TEXT, cidade TEXT, estado TEXT, uf TEXT, email TEXT, telefone TEXT, data_nascimento DATE, data_cadastro DATE)");
             statement.execute("CREATE TABLE IF NOT EXISTS livro_emprestimo (id_livro_emprestimo INTEGER PRIMARY KEY, id_livro INTEGER, id_pessoa INTEGER, multa BOOLEAN, data_emprestimo DATE, data_devolucao DATE, FOREIGN KEY (id_livro) REFERENCES livro(id_livro), FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa))");
-            statement.execute("CREATE TABLE IF NOT EXISTS colaborador (id_colaborador INTEGER PRIMARY KEY, cargo TEXT, id_pessoa INTEGER  data_registro DATE, pis TEXT, rg TEXT, senha TEXT, FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa))");
-//            connection.close();
+            statement.execute("CREATE TABLE IF NOT EXISTS colaborador (id_colaborador INTEGER PRIMARY KEY, cargo TEXT, id_pessoa INTEGER, data_registro DATE, pis TEXT, rg TEXT, senha TEXT, FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa))");
+            
+            connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            System.out.println("Database created successfully");
         }
     }
 }
